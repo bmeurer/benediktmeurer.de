0b1717ec0af3d876aa8a2370f26afed8c7e21fa5
Benedikt Meurer <bmeurer@google.com>
2018-12-02 20:35:04 +0100

    [js] Automatically insert bookmark anchors for h2-h6.

--- js/main.js
+++ js/main.js
@@ -19,6 +19,20 @@
     history.replaceState({}, '', location.pathname);
   }
 
+  // Append bookmark links to h2-h6 inside of posts.
+  window.addEventListener("load", function(event) {
+    document.querySelectorAll(".post h2, .post h3, .post h4, .post h5, .post h6").forEach(function(h) {
+      var t = document.createTextNode(" ");
+      h.appendChild(t);
+      var a = document.createElement("a");
+      a.href = "#" + h.id;
+      a.text = "#";
+      a.setAttribute("aria-hidden", "true")
+      a.setAttribute("class", "bookmark")
+      h.appendChild(a);
+    });
+  });
+
   // Google Analytics.
   var UA_ID = 'UA-24476004-1';
   self.dataLayer = [];
