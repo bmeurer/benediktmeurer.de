69c30ccb357d16ebccd2073e2713307ae96253ee
Benedikt Meurer <bmeurer@google.com>
2018-12-06 13:22:01 +0100

    [sw] Properly cache the Google Fonts we're using.

--- sw.js
+++ sw.js
@@ -16,12 +16,33 @@ workbox.routing.setDefaultHandler(workbox.strategies.networkFirst());
 // let Workbox handle our precache list
 workbox.precaching.precacheAndRoute([]);
 
-// use `Stale-while-revalidate` strategy for images and fonts.
+// use stale-while-revalidate strategy for our images.
 workbox.routing.registerRoute(
     /images/,
     workbox.strategies.staleWhileRevalidate()
 );
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
 workbox.routing.registerRoute(
-    /^https?:\/\/fonts\.googleapis\.com/,
-    workbox.strategies.staleWhileRevalidate()
+  /^https:\/\/fonts\.googleapis\.com/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  workbox.strategies.cacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
 );
